116e2ba2683cdbe1198e79ec17edd44bc70fcd12
Tomás Solar Castro <tsolar@gmail.com>
2015-07-23 19:08:11 -0300

    fix STATIC vars

--- mediamapper/settings.py
+++ mediamapper/settings.py
@@ -138,11 +138,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
-STATIC_ROOT = BASE_DIR + '/mediamapper/static/'
+STATIC_ROOT = BASE_DIR + '/static/'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    BASE_DIR + "/mediamapper/static/medioschile/",
+    BASE_DIR + "/static/medioschile/",
 )
 
 ENV_PATH = os.path.abspath(os.path.dirname(__file__))
